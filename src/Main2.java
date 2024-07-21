ccac2864724b6abb2d1b96139f431474c671c68a
KareemZafan <kareemm.zafan@gmail.com>
2024-07-21 12:38:19 +0300

    feature-44 cleaning up Main2.java

--- src/Main2.java
+++ src/Main2.java
@@ -4,10 +4,7 @@ public class Main2{
 
  public static void main(String[] args){
  for(int i = 0 ; i < 10; ++i)
-   System.out.println("Configuration Management Second Day"); 
-System.out.println("Configuration Management Second Day1"); 
-System.out.println("Configuration Management Second Day2"); 
-System.out.println("Configuration Management Second Day3"); 
+   System.out.println("Configuration Management Second Day"+(i+1)); 
   }
 
 }
