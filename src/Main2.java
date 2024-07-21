acf9cb131e7865b6a04864ec4fd853b2300abe42
KareemZafan <kareemm.zafan@gmail.com>
2024-07-21 12:47:22 +0300

    applying review comments

--- src/Main2.java
+++ src/Main2.java
@@ -3,8 +3,10 @@
 public class Main2{
 
  public static void main(String[] args){
- for(int i = 0 ; i < 10; ++i)
-   System.out.println("Configuration Management Second Day"+(i+1)); 
+ for(int i = 0 ; i < 3; ++i)
+ 	{
+  	 	System.out.println("Configuration Management Second Day"+(i+1)); 
+ 	}
   }
 
 }
