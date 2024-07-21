51a0c824ddadd35c9a3afc6c97780202ba9c68db
Ali-Abdelhakem-00 <aliabdelhakem00@gmail.com>
2024-07-21 12:18:39 +0300

    update

--- src/Main2.java
+++ src/Main2.java
@@ -3,7 +3,7 @@
 public class Main2{
 
  public static void main(String[] args){
- for(int i = 0 ; i < 10; ++i)
+ for(int i = 0 ; i < 3; ++i)
    System.out.println("Configuration Management Second Day"); 
   }
 
